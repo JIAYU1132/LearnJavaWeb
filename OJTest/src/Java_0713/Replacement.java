08b41f0ce7eec1cb046930974c2992eecb7b7423
yjy <386902834@qq.com>
2020-07-14 08:11:07 +0800

    最近公共祖先/替换空格

---
+++ OJTest/src/Java_0713/Replacement.java
@@ -0,0 +1,38 @@
+package Java_0713;
+
+/**
+ *空格替换
+ *
+ * 请编写一个方法，将字符串中的空格全部替换为“%20”。假定该字符串有足够的空间存放新增的字符，并且知道字符串的真实长度(小于等于1000)，同时保证字符串由大小写的英文字母组成。
+ *
+ * 给定一个string iniString 为原始的串，以及串的长度 int len, 返回替换后的string。
+ *
+ * 测试样例：
+ * "Mr John Smith”,13
+ * 返回："Mr%20John%20Smith"
+ * ”Hello  World”,12
+ * 返回：”Hello%20%20World”
+ */
+public class Replacement {
+    //使用StringBuilder
+    public String replaceSpace(String iniString, int length) {
+        if (iniString == null || length <= 0) {
+            return iniString;
+        }
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < length; i++) {
+            char c = iniString.charAt(i);
+            if (c == ' ') {
+                sb.append("%20");
+            } else {
+                sb.append(c);
+            }
+        }
+        return sb.toString();
+    }
+
+    // 直接替换
+    public String replaceSpace2(String iniString, int length) {
+        return iniString.replaceAll(" ", "20%");
+    }
+}
