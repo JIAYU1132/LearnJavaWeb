70b57dccc0eafda81ef4aabad01160c87d5a3081
yjy <386902834@qq.com>
2020-08-19 09:09:46 +0800

    OJTest

---
+++ OJTest/src/Java06_28/Main.java
@@ -0,0 +1,10 @@
+package Java06_28;
+
+public class Main {
+    private float f=1.0f;
+    int m=12;
+    static int n=1;
+    public static void main(String args[]){
+        Main t=new Main();
+    }
+}
